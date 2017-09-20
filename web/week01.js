b77b65a55eea50c02c1e5e4019393b2efcf0abe0
mosiah10Z <que14003@byui.edu>
2017-09-19 22:13:16 -0600

    changes

--- web/week01.js
+++ web/week01.js
@@ -2,7 +2,21 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Clicked";
 }
 
-function changeColor() {
-	var x = document.getElementById("input")value;
+function changeCeolor() {
+	var x = document.getElementById("input").value;
 	document.getElementById("one").style.backgroundColor = '"' + x + '"';
+	console.log(x);
+}
+
+function changeColor() {
+	var textbox_id = "input";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "two";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
+
 }
