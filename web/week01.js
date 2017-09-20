eb29e2a2b65ec3b6bb1c673635e4c2a2ad83a248
mosiah10Z <que14003@byui.edu>
2017-09-19 20:49:03 -0600

    lame

---
+++ web/week01.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Clicked";
+}
+
+function changeColor() {
+	var x = document.getElementById("input")value;
+	document.getElementById("one").style.backgroundColor = '"' + x + '"';
+}
