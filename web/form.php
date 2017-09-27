e067e8b7aeeeddfd4eb83d98c8d5bd3bf0de244e
mosiah10Z <que14003@byui.edu>
2017-09-27 10:59:23 -0600

    comment

---
+++ web/form.php
@@ -0,0 +1,19 @@
+<?php
+// define variables and set to empty values
+$name = $email = $major = $comment = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $major = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+}
+?>
